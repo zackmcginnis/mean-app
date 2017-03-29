eda35edbba68ebba626b7509616e396afacdde1c
zackmcginnis <zm3@pdx.edu>
2017-03-29 14:54:54 -0700

    connected mongodb with mlab

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/meanauth',
+  //database: 'mongodb://localhost:27017/meanauth',
+  database: 'mongodb://zack:zack@ds145790.mlab.com:45790/vacation-calc',
   secret: 'yoursecret'
 }
