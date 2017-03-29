210b743495a7dba17b064094ee62a47f23d8db4d
zackmcginnis <zm3@pdx.edu>
2017-03-29 15:19:09 -0700

    fix

--- models/user.js
+++ models/user.js
@@ -87,7 +87,7 @@ module.exports.addUser = function(newUser, callback){
   bcrypt.genSalt(10, (err, salt) => {
     bcrypt.hash(newUser.password, salt, (err, hash) => {
       if(err) {
-        throw err;
+        console.log(err);
       }
       newUser.password = hash;
       newUser.save(callback);
