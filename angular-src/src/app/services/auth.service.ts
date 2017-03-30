6a39563e1d10ad22de900504365875eda3f271cf
zackmcginnis <zm3@pdx.edu>
2017-03-30 08:30:52 -0700

    new fix

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -13,22 +13,27 @@ export class AuthService {
   user: any;
   vacations: any;
   guest: any;
+  isDep: boolean;
 
-  constructor(private http:Http) { }
+  constructor(private http:Http) { 
+    this.isDep = true;  //change to false if developing locally
+  }
 
   registerUser(user){
     let headers = new Headers();
     headers.append('Content-Type','application/json');
     console.log("browser...", user)
+    let ep = this.prepEndpoint('users/register');
     //return this.http.post('http://localhost:3000/users/register', user,{headers: headers})
-    return this.http.post('users/register', user,{headers: headers})
+    return this.http.post(ep, user,{headers: headers})
       .map(res => res.json());
   }
 
   authenticateUser(user){
     let headers = new Headers();
     headers.append('Content-Type','application/json');
-    return this.http.post('users/authenticate', user,{headers: headers})
+    let ep = this.prepEndpoint('users/authenticate');
+    return this.http.post(ep, user,{headers: headers})
       .map(res => res.json());
   }
 
@@ -37,7 +42,8 @@ export class AuthService {
     this.loadToken();
     headers.append('Authorization', this.authToken);
     headers.append('Content-Type','application/json');
-    return this.http.get('users/profile',{headers: headers})
+    let ep = this.prepEndpoint('users/profile');
+    return this.http.get(ep,{headers: headers})
       .map(res => res.json());
   }
 
@@ -69,9 +75,10 @@ export class AuthService {
     this.loadToken();
     vacation.newFlag = false;
     console.log(vacation)
+    let ep = this.prepEndpoint('users/vacations');
     headers.append('Authorization', this.authToken);
     headers.append('Content-Type','application/json');
-    return this.http.post('users/vacations', vacation, {headers: headers})
+    return this.http.post(ep, vacation, {headers: headers})
       .map(res => res.json())
       .subscribe(
         data => console.log(data),
@@ -83,18 +90,20 @@ export class AuthService {
   getVacations(): Observable<any> {
     let headers = new Headers();
     this.loadToken();
+    let ep = this.prepEndpoint('users/vacations');
     headers.append('Authorization', this.authToken);
     headers.append('Content-Type','application/json');
-    return this.http.get('users/vacations',{headers: headers})
+    return this.http.get(ep,{headers: headers})
       .map(res => res.json())
   }
 
   updateVacation(vacation){
     let headers = new Headers();
     this.loadToken();
+    let ep = this.prepEndpoint('users/vacations/edit');
     headers.append('Authorization', this.authToken);
     headers.append('Content-Type','application/json');
-    return this.http.put('users/vacations/edit', vacation, {headers: headers})
+    return this.http.put(ep, vacation, {headers: headers})
       .map(res => res.json())
       .subscribe(
         data => console.log(data),
@@ -106,10 +115,11 @@ export class AuthService {
   deleteVacation(vacation){
     let headers = new Headers();
     this.loadToken();
+    let ep = this.prepEndpoint('users/vacations/delete');
     headers.append('Authorization', this.authToken);
     console.log("deleting this vacation", vacation)
     headers.append('Content-Type','application/json');
-    return this.http.put('users/vacations/delete', vacation, {headers: headers})
+    return this.http.put(ep, vacation, {headers: headers})
       .map(res => res.json())
       .subscribe(
         data => console.log(data),
@@ -122,37 +132,50 @@ export class AuthService {
   addGuest(guest){
     let headers = new Headers();
     this.loadToken();
+    let ep = this.prepEndpoint('users/vacations/guests');
     headers.append('Authorization', this.authToken);
     headers.append('Content-Type','application/json');
-    return this.http.post('users/vacations/guests', guest,{headers: headers})
+    return this.http.post(ep, guest,{headers: headers})
       .map(res => res.json());
   }
 
   getGuests(): Observable<any>{
     let headers = new Headers();
     this.loadToken();
+    let ep = this.prepEndpoint('users/vacations/guests');
     headers.append('Authorization', this.authToken);
     headers.append('Content-Type','application/json');
-    return this.http.get('users/vacations/guests',{headers: headers})
+    return this.http.get(ep,{headers: headers})
       .map(res => res.json());
   }
 
   updateGuests(guest){
     let headers = new Headers();
     this.loadToken();
+    let ep = this.prepEndpoint('users/vacations');
     headers.append('Authorization', this.authToken);
     headers.append('Content-Type','application/json');
-    return this.http.get('users/vacations',{headers: headers})
+    return this.http.get(ep,{headers: headers})
       .map(res => res.json());
   }
 
   deleteGuest(guest){
     let headers = new Headers();
     this.loadToken();
+    let ep = this.prepEndpoint('users/vacations');
     headers.append('Authorization', this.authToken);
     headers.append('Content-Type','application/json');
-    return this.http.get('users/vacations',{headers: headers})
+    return this.http.get(ep,{headers: headers})
       .map(res => res.json());
   }
+
+  prepEndpoint(ep){
+    if(this.isDep){
+      return ep;
+    } else {
+      //return 'http://localhost:8080/'+ep;
+      return 'http://localhost:3000/'+ep;
+    }
+  }  
 
 }
